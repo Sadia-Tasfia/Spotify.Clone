66fd197c9b9a197074946f6f73a7e897f1e9b7d5
Sadia-Tasfia <tasfia.sujana2903@gmail.com>
2023-09-26 00:10:45 +0600

    js added

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Welcome to Spotify");
+
+// Iniatialize the variable
+let songs = [
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/1.jpg"},
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/2.jpg"},
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/3.jpg"},
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/4.jpg"},
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/5.jpg"},
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/6.jpg"},
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/7.jpg"},
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/8.jpg"},
+  {songName:"Let me love", filePath:"songs/1.mp3", coverPath: "cover/9.jpg"},
+]
+
+let audio = new Audio("1.mp3");
+// audioElement.play();
